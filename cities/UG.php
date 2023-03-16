7bf4a68d5d75edad6bb2aa12fc85698eb41dd6fd
Osward Nasasira <108095830+oswardna@users.noreply.github.com>
2023-03-16 22:38:15 +0300

    Files uploaded

---
+++ cities/UG.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Ugandan cities
+ */
+global $cities;
+
+$cities['UG'] = array(
+        'UG314' => array(
+            'Sakodi',
+            'Bluetag'
+        ),
+/**
+ * Kampala
+ */
+        'UG102' => array(
+            'Nakawa',
+            'Kampala',
+            'Bugolobi',
+            'Bukoto',
+            'Bulambiro',
+            'Buye',
+            'Bwaise',
+            'Gaba',
+            'Kawempe',
+            'Kigowa',
+            'Kisaasi',
+            'Kyambogo',
+            'Kyebando',
+            'Lugala',
+            'Luzira',
+            'Makerere',
+            'Makindye',
+            'Mbuya',
+            'Mengo',
+            'Mpanga',
+            'Mulago',
+            'Munyonyo',
+            'Nakawa',
+            'Namirembe',
+            'Port Bell',
+            'Kitintale'
+        )
+    );
